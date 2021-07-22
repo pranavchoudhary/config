8114d1a5d8a35ec2f2210efc18abdbd9e7a7bfc5
Pranav Choudhary <pranav@arista.com>
2021-07-22 03:26:54 -0700

    Setting mode to vi for everything that uses readline

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ mkdir -p ~/.config/nvim
 ln -s $(pwd)/nvim/init.vim ~/.config/nvim/init.vim
 ln -s $(pwd)/nvim/plugins.vim ~/.config/nvim/plugins.vim
 ln -s $(pwd)/tmux.conf ~/.tmux.conf
+ln -s $(pwd)/inputrc ~/.inputrc
 
 # Setup global git config
 git config --global core.editor "nvim"
